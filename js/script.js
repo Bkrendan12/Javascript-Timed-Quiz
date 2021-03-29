cd3c5182a2c6f7036e4f1c3b44f2d3a23c44a19a
Brendan Tyler <bkrendan12@gmail.com>
2021-03-29 14:26:11 -0400

    downsized my html, css, javascript

--- js/script.js
+++ js/script.js
@@ -1,89 +1,64 @@
-var startBtn = document.getElementById("start");
+var startBtn = document.querySelector("#start");
 var questionsBox = document.querySelector(".questionsDiv");
-var quizBox = document.getElementById("quiz-container");
+var quizContainer = document.getElementById("quiz-container");
 var highScoreBtn = document.querySelector(".Highscores");
 
-var questions = [];
-var choices = [];
-var answers = [];
-var score = 0;
+var questionTitle = document.querySelector("#question");
 
-// loop thru the questions and if answer is c push the value to answers
+var test = document.querySelector("#question");
 
 
 
-
-    var questions = [  
+var questionsArray = [  
         {   
          question: "What does parseInt do?",
      
-         a: "parses a string argument and returns a floating point number.",
-         b: "evaluates JavaScript code represented as a string.",
-         c: "parses a string argument and returns an integer of the specified radix",
+         choice1: "parses a string argument and returns a floating point number.",
+         choice2: "evaluates JavaScript code represented as a string.",
+         choice3: "parses a string argument and returns an integer of the specified radix",
+         choice4: "it pops up a prompt window in your browser.",
      
-         answer: "C"
+         answer: 3,
         },
      
      {
          question: "how to do declare a variable?",
      
-         a: "variable name = value",
-         b: "var NAME = value;",
-         c: "var name = value;",
+         choice1: "variable name = value",
+         choice2: "var NAME = value;",
+         choice3: "var name = value;",
+         choice4: "var = name.value;",
      
-         answer: "C"
+         answer: 3,
         },
      
      {
          question: "which of these will evaluate to the number 20?",
      
-         a: "var twenty = 10 + 10; ",
-         b: "var twenty = '10' + '10'; ",
-         c: "var twenty = 10 + '10'; ",
+         choice1: "twenty = '20' ",
+         choice2: "var twenty = '10' + '10'; ",
+         choice3: "var twenty = 10 + '10'; ",
+         choice4: "var twenty = 10 + 10; ",
      
-         answer: "A"
+         answer: 4,
         },
      
      {
          question: "what is the proper syntax for this function?",
      
-         a: "myfunction() {console.log('Hello World!';}; ",
-         b: "function MYFUNCTION {console.log('Hello World!';}; ",
-         c: "function myFunction() {console.log('Hello World!');}; ",
-     
-         answer: "C"
+         choice1: "myfunction() (console.log('Hello World!'); ",
+         choice2: "function MYFUNCTION {console.log('Hello World!'}; ",
+         choice3: "function myFunction() {console.log('Hello World!')}; ",
+         choice4: "function() = myFunction {console.log(12)} ",
+
+         answer: 3,
         }
      ];
 
-     var questions =
-     [
-       {
-           question: "What is the capital of United Kingdom?",
-           choices: ["Manchester", "Birmingham", "London", "Birmingham"],
-           answer: 2
-       },
 
-       {
-           question: "What is the capital of United States?",
-           choices: ["California", "New York", "Miami", "Florida"],
-           answer: 1
-       }
-
-
-     ];
-
-
-
-
-var startBtn = document.getElementById("start");
-var questionsBox = document.querySelector(".questionsDiv");
-var quizBox = document.getElementById("quiz-container");
-var highScoreBtn = document.querySelector(".Highscores");
-
-
-document.querySelector('#start').addEventListener('click',function(){
+startBtn.addEventListener('click',function(){
     startBtn.style.display='none';
-
+ 
     let counter = 5;
 
     setInterval(function() {
@@ -95,9 +70,11 @@ document.querySelector('#start').addEventListener('click',function(){
         if (counter === 0) {
             countdown.innerHTML = "End";
             clearInterval(setInterval);
-            quizBox.setAttribute("style", "width: 250px; height: 150px");
+            questionsBox.setAttribute("style", "width: 250px; height: 150px");
             questionsBox.style.display = 'none';
             highScoreBtn.style.visibility = 'visible';
+        
+            
         }
     }, 1000);
   
@@ -115,16 +92,53 @@ document.querySelector('#start').addEventListener('click',function(){
 
 
 
+    //  let SCORE_POINTS = 100;
+    //  let MAX_QUESTIONS = 4;
 
+    //  function startGame() {
+    //     questionCounter = 0;
+    //     score = 0;
+    //     availableQuestions = [...questions];
+    //     getNewQuestion();   
+    //  }
+ 
+    //  function getNewQuestion() {
+    //      if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+    //          localStorage.setItem('mostRecentScore', score)
+    //          return window.location.assign('/end.html')
+    //      }
 
+    //      questionCounter++
+    //      let questionIndex = Math.floor(Math.random() * availableQuestions.length)
+    //      currentQuestion = availableQuestions[questionIndex]
+    //      question.innertext = currentQuestion.question
+    //      choices.forEach(choice => {
+    //          let number = choice.dataset['number']
+    //          choice.innertext = currentQuestion['choice']
+    //      })
 
+    //      availableQuestions.splice(questionIndex, 1)
 
+    //      acceptingAnswers = true;
+    //  }
 
+    //  choices.forEach(choice => {
+    //      choice.addEventListener('click', e => {
+    //          if(!acceptingAnswers)
+    //          return
+    //          acceptingAnswers = false;
+    //          let selectedChoice = e.target
+    //          let selectedAnswers = selectedChoice.dataset['number'];
+    //          let classToApply = selectedAnswer == currentQuestion.asnwer ? 'correct' : 'incorrect'
 
+    //          if(classToApply === 'correct') {
+    //              incrementScore(SCORE_POINTS);
 
+    //          }
 
-
-
+    //          selectedChoice.parentElement.classList.add(classToApply)
+    //      })
+    //  })
 
 
 
